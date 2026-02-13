db250f6f9b0f0d242132477415572a8316a53666
macaronquantum <quantumiaqtm@proton.me>
2026-02-13 21:26:06 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,11 +2,17 @@
 
 echo "===== STARTING APPLICATION ====="
 
-echo "Installing dependencies..."
-npm install  # <-- PAS npm ci
+echo "Installing backend dependencies..."
+cd backend
+npm install || echo "No npm backend dependencies"
+
+echo "Installing frontend dependencies..."
+cd ../frontend
+npm install
 
 echo "Building frontend..."
 npm run build
 
-echo "Starting application..."
-npm run start
+echo "Starting backend..."
+cd ../backend
+npm start  # ou python app.py selon ton backend
